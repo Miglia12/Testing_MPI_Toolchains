7bcb6f2fd6de0b5ba6778b746af2c369f3d16025
Miglia12 <lorenzo1.migliari@mail.polimi.it>
2024-05-21 21:55:19 +0200

    First sucessfull test

--- tests/base/new.py
+++ tests/base/new.py
@@ -1,18 +1,16 @@
-from hpctestlib.microbenchmarks.mpi.osu import osu_benchmark
-
 import reframe as rfm
 import reframe.utility.sanity as sn
 import os
 
+@rfm.simple_test
 class fetch_osu_benchmarks(rfm.RunOnlyRegressionTest):
     valid_systems = ['aion:batch']
     valid_prog_environs = ['*']
-
     descr = 'Fetch OSU benchmarks'
     version = variable(str, value='7.4')
     executable = 'wget'
     executable_opts = [
-        f'http://mvapich.cse.ohio-state.edu/download/mvapich/osu-micro-benchmarks-{version}.tar.gz'  # noqa: E501
+        f'http://mvapich.cse.ohio-state.edu/download/mvapich/osu-micro-benchmarks-{version}.tar.gz'
     ]
     local = True
 
@@ -20,11 +18,10 @@ class fetch_osu_benchmarks(rfm.RunOnlyRegressionTest):
     def validate_download(self):
         return sn.assert_eq(self.job.exitcode, 0)
 
-
+@rfm.simple_test
 class build_osu_benchmarks(rfm.CompileOnlyRegressionTest):
     valid_systems = ['aion:batch']
     valid_prog_environs = ['*']
-
     descr = 'Build OSU benchmarks'
     build_system = 'Autotools'
     build_prefix = variable(str)
@@ -33,8 +30,7 @@ class build_osu_benchmarks(rfm.CompileOnlyRegressionTest):
     @run_before('compile')
     def prepare_build(self):
         tarball = f'osu-micro-benchmarks-{self.osu_benchmarks.version}.tar.gz'
-        self.build_prefix = tarball[:-7]  # remove .tar.gz extension
-
+        self.build_prefix = tarball[:-7]
         fullpath = os.path.join(self.osu_benchmarks.stagedir, tarball)
         self.prebuild_cmds = [
             f'cp {fullpath} {self.stagedir}',
@@ -45,39 +41,31 @@ class build_osu_benchmarks(rfm.CompileOnlyRegressionTest):
 
     @sanity_function
     def validate_build(self):
-        # If compilation fails, the test would fail in any case, so nothing to
-        # further validate here.
         return True
 
-
-class OSUBenchmarkTestBase(rfm.RunOnlyRegressionTest):
-    '''Base class of OSU benchmarks runtime tests'''
-
-    valid_systems = ['*']
+@rfm.simple_test
+class run_osu_benchmarks(rfm.RunOnlyRegressionTest):
+    valid_systems = ['aion:batch']
     valid_prog_environs = ['*']
-    num_tasks = 2
-    num_tasks_per_node = 1
-    osu_binaries = fixture(build_osu_benchmarks, scope='environment')
+    descr = 'Run OSU benchmarks'
+    osu_benchmarks = fixture(build_osu_benchmarks, scope='environment')
 
-    # @sanity_function
-    # def validate_test(self):
-    #     # return sn.assert_found(r'^8', self.stdout)
-    #     return True
+    @run_before('run')
+    def set_executable(self):
+        self.executable = os.path.join(
+            self.osu_benchmarks.stagedir,
+            'osu-micro-benchmarks-7.4',
+            'c',
+            'mpi',
+            'startup',
+            'osu_hello'
+        )
 
+    @sanity_function
+    def validate_run(self):
+        return sn.assert_found('# OSU MPI Hello World Test', self.stdout)
 
-# @rfm.simple_test
-# class osu_latency_test(OSUBenchmarkTestBase):
-#     descr = 'OSU latency test'
-
-#     @run_before('run')
-#     def prepare_run(self):
-#         self.executable = os.path.join(
-#             self.osu_binaries.stagedir,
-#             self.osu_binaries.build_prefix,
-#             'c', 'mpi', 'pt2pt', 'osu_latency'
-#         )
-#         self.executable_opts = ['-x', '100', '-i', '1000']
-
-#     @performance_function('us')
-#     def latency(self):
-#         return sn.extractsingle(r'^8\s+(\S+)', self.stdout, 1, float)
+    @run_after('run')
+    def unload_module(self):
+        modules_to_unload = self.current_environ.modules
+        self.postrun_cmds = [f'module unload {module}' for module in modules_to_unload]
