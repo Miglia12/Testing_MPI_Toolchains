933ab3c51dccb564e1dbfa6f38be043c252b9e02
pannalukacs <pannalukacs00@gmail.com>
2024-05-22 18:33:53 +0200

    Actually cleaned up

--- tests/base/osu_base.py
+++ tests/base/osu_base.py
@@ -2,7 +2,7 @@ import reframe as rfm
 import reframe.utility.sanity as sn
 import os
 
-@rfm.simple_test
+
 class fetch_osu_benchmarks(rfm.RunOnlyRegressionTest):
     valid_systems = ['aion:batch']
     valid_prog_environs = ['*']
@@ -18,7 +18,7 @@ class fetch_osu_benchmarks(rfm.RunOnlyRegressionTest):
     def validate_download(self):
         return sn.assert_eq(self.job.exitcode, 0)
 
-@rfm.simple_test
+
 class build_osu_benchmarks(rfm.CompileOnlyRegressionTest):
     valid_systems = ['aion:batch']
     valid_prog_environs = ['*']
@@ -38,34 +38,24 @@ class build_osu_benchmarks(rfm.CompileOnlyRegressionTest):
             f'cd {self.build_prefix}'
         ]
         self.build_system.max_concurrency = 8
-
+    
     @sanity_function
     def validate_build(self):
         return True
 
-@rfm.simple_test
-class run_osu_benchmarks(rfm.RunOnlyRegressionTest):
+    
+class OSUBenchmarkBase(rfm.RunOnlyRegressionTest):
     valid_systems = ['aion:batch']
     valid_prog_environs = ['*']
     descr = 'Run OSU benchmarks'
     osu_benchmarks = fixture(build_osu_benchmarks, scope='environment')
 
     @run_before('run')
-    def set_executable(self):
-        self.executable = os.path.join(
-            self.osu_benchmarks.stagedir,
-            'osu-micro-benchmarks-7.4',
-            'c',
-            'mpi',
-            'startup',
-            'osu_hello'
-        )
-
-    @sanity_function
-    def validate_run(self):
-        return sn.assert_found('# OSU MPI Hello World Test', self.stdout)
-
-    @run_after('run')
-    def unload_module(self):
-        modules_to_unload = self.current_environ.modules
-        self.postrun_cmds = [f'module unload {module}' for module in modules_to_unload]
+    def set_computational_resources(self):
+        self.job.options = [
+            '--time=00:05:00',
+            '--nodes=1',
+            '--ntasks=2',  
+            '--ntasks-per-node=2',  
+            '--cpus-per-task=1'
+        ]
--- tests/pt2pt.py
+++ tests/pt2pt.py
@@ -1,28 +1,43 @@
-from base.osu_benchmarks_base import OSU_Micro_Benchmark
+from base.osu_base import OSUBenchmarkBase
+from os import path
 
 import reframe as rfm
 import reframe.utility.sanity as sn
 
 
 @rfm.simple_test
-class LatencyTest(OSU_Micro_Benchmark):
-    
-    def __init__(self):
-        super().__init__()           
-        self.executable = self.paths['latency'] 
+class test_latency(OSUBenchmarkBase):
+    @run_before('run')
+    def set_executable(self):
+        self.executable = path.join(
+            self.osu_benchmarks.stagedir,
+            'osu-micro-benchmarks-7.4',
+            'c',
+            'mpi',
+            'pt2pt',
+            'standard',
+            'osu_latency'
+        )
 
     @sanity_function
-    def validate(self):
-        return sn.assert_found(r'OSU MPI Latency Test', self.stdout)
+    def validate_run(self):
+        return sn.assert_found('OSU MPI Latency Test', self.stdout)
     
 
 @rfm.simple_test
-class BandwidthTest(OSU_Micro_Benchmark):
-    
-    def __init__(self):
-        super().__init__()           
-        self.executable = self.paths['bw']
+class test_bandwidth(OSUBenchmarkBase):
+    @run_before('run')
+    def set_executable(self):
+        self.executable = path.join(
+            self.osu_benchmarks.stagedir,
+            'osu-micro-benchmarks-7.4',
+            'c',
+            'mpi',
+            'pt2pt',
+            'standard',
+            'osu_bw'
+        )
 
     @sanity_function
-    def validate(self):
-        return sn.assert_found(r'OSU MPI Bandwidth Test', self.stdout)
+    def validate_run(self):
+        return sn.assert_found('OSU MPI Bandwidth Test', self.stdout)
